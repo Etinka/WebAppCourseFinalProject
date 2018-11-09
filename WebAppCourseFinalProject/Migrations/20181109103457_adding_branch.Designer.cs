89193b4557f16ec7442725d8a7e9149d27538d21
Etinka <etinegev@gmail.com>
2018-11-09 12:19:05 +0200

    Update to DB of branch

---
+++ WebAppCourseFinalProject/Migrations/20181109103457_adding_branch.Designer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WebAppCourseFinalProject.Models;
+
+namespace WebAppCourseFinalProject.Migrations
+{
+    [DbContext(typeof(UserContext))]
+    [Migration("20181109103457_adding_branch")]
+    partial class adding_branch
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.5")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebAppCourseFinalProject.Models.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Title")
+                        .IsRequired();
+
+                    b.Property<string>("Subtitle")
+                        .IsRequired();
+
+
+                    b.Property<double>("Longtitude")
+                        .IsRequired();
+
+                    b.Property<double>("Latitude")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Branch");
+                });
+        }
+    }
+}
---
+++ WebAppCourseFinalProject/Migrations/20181109103457_adding_branch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAppCourseFinalProject.Migrations
+{
+    public partial class adding_branch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Branch",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: false),
+                    Longtitude = table.Column<double>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branch", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Branch");
+        }
+    }
+}
